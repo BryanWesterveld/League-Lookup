d4c4bb5eb577d92d34ddb3dae482e556d0910cb7
Bryan Westerveld <bryan.westerveld@gmail.com>
2018-04-29 17:05:21 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import registerServiceWorker from './registerServiceWorker';
+import promise from "redux-promise";
+
+import App from './components/app';
+import reducers from "./reducers";
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={App} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>
+  , document.getElementById('root'));
+
+registerServiceWorker();
