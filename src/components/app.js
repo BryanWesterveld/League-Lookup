6db2b81cc72b4468d66a5cc52ef1c8490e2a1da7
Bryan Westerveld <bryan.westerveld@gmail.com>
2018-04-29 17:50:09 +0200

    Update .gitignore

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
+import { API_KEY } from "../hidden/api_keys";
 
 class App extends Component {
   render() {
-    return <div>Basic component</div>;
+    return <div>Basic component, key is: {API_KEY}</div>;
   }
 }
 
